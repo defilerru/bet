cd7635634675baff07081d1c34c1f8957dade2d9
Batman <sp4mb0t@mail.ru>
2021-04-22 03:38:21 +0300

    Basic mysql storage implementation

--- mysql.go
+++ mysql.go
@@ -1,1 +1,112 @@
 package bet
+
+import (
+	"context"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+type MySQLDB struct {
+	db                   *sql.DB
+	stmtCreatePrediction *sql.Stmt
+	stmtSelectPrediction *sql.Stmt
+}
+
+const (
+	stmtCreatePrediction = "INSERT INTO predictions(name, option_1, option_2, start_delay_seconds) VALUES(?,?,?,?)"
+	stmtSelectPrediction = "SELECT created_at, started_at FROM predictions WHERE id = ?"
+	stmtCreateBet        = "INSERT INTO bets(user_id, prediction_id, amount, on_first_option) VALUES(?,?,?,?)"
+	stmtTakePayment      = "UPDATE bets_users SET balance=balance-? WHERE id=? AND balance>=?"
+)
+
+func NewMySQLDB(dataSourceName string) (*MySQLDB, error) {
+	db, err := sql.Open("mysql", dataSourceName)
+	mysqlDB := &MySQLDB{
+		db: db,
+	}
+	if err != nil {
+		return mysqlDB, err
+	}
+
+	mysqlDB.stmtCreatePrediction, err = db.Prepare(stmtCreatePrediction)
+	if err != nil {
+		return mysqlDB, err
+	}
+
+	mysqlDB.stmtSelectPrediction, err = db.Prepare(stmtSelectPrediction)
+	if err != nil {
+		return mysqlDB, err
+	}
+
+	return mysqlDB, err
+}
+
+func (m *MySQLDB) CreatePrediction(prediction *Prediction) error {
+	res, err := m.stmtCreatePrediction.Exec(prediction.Name, prediction.Opt1, prediction.Opt2, prediction.StartDelaySeconds)
+	if err != nil {
+		return err
+	}
+	rowID, err := res.LastInsertId()
+	prediction.Id = uint64(rowID)
+	return err
+}
+
+func (m *MySQLDB) LoadPrediction(id uint64) (*Prediction, error) {
+	p := &Prediction{}
+	nt := sql.NullTime{}
+	err := m.stmtSelectPrediction.QueryRow(id).Scan(&p.CreatedAt, &nt)
+	p.StartedAt = nt.Time
+	//TODO: the rest
+	return p, err
+}
+
+func (m *MySQLDB) ClosePrediction(prediction *Prediction, opt1Won bool) error {
+	tx, err := m.db.BeginTx(context.TODO(), nil)
+	if err != nil {
+		return err
+	}
+	for _, bet := range prediction.Bets {
+		log.Println(bet)
+	}
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *MySQLDB) CreateBet(prediction *Prediction, bet *Bet) error {
+	tx, err := m.db.BeginTx(context.TODO(), nil)
+	if err != nil {
+		return err
+	}
+	stmtPay, err := tx.Prepare(stmtTakePayment)
+	if err != nil {
+		return err
+	}
+	res, err := stmtPay.Exec(bet.Amount, bet.UserId, bet.Amount)
+	if err != nil {
+		return err
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rows == 0 {
+		return &Insufficient{}
+	}
+	stmt, err := tx.Prepare(stmtCreateBet)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(bet.UserId, prediction.Id, bet.Amount, bet.OnFirstOption)
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
+}
+
+func (m *MySQLDB) DeleteBet(prediction *Prediction, uid UID) error {
+	return nil
+}
--- mysql_test.go
+++ mysql_test.go
@@ -1,1 +1,60 @@
 package bet
+
+import (
+	"flag"
+	"testing"
+)
+
+var skipmysql = flag.Bool("skipmysql", false, "only perform local tests")
+
+func assertBool(actual bool, expected bool, t *testing.T) {
+	if actual != expected {
+		t.Fatalf("bool fail %t != %t", actual, expected)
+	}
+}
+
+func TestCreateClosePrediction(t *testing.T) {
+	if *skipmysql {
+		t.Skip("skipmysql flag is not set")
+	}
+	db, err := NewMySQLDB("defiler@/defiler?parseTime=true&loc=Local")
+	if err != nil {
+		t.Fatalf("can't connect to skipmysql: %s", err)
+	}
+	prediction, err := CreatePrediction("Test 1", "Yes", "No", 40, db)
+	err = db.CreatePrediction(prediction)
+	if err != nil {
+		t.Fatalf("can't create prediction: %s", err)
+	}
+
+	err = prediction.AddBet(Bet{
+		UserId:        41,
+		Amount:        200,
+		OnFirstOption: false,
+	})
+	assertNoError(err, t)
+
+	err = prediction.AddBet(Bet{
+		UserId:        41,
+		Amount:        200,
+		OnFirstOption: false,
+	})
+	assertErrorInstance(err, &Duplicate{}, t)
+
+	err = prediction.AddBet(Bet{
+		UserId:        42,
+		Amount:        200000,
+		OnFirstOption: false,
+	})
+	assertErrorInstance(err, &Insufficient{}, t)
+
+	predictionLoaded, err := db.LoadPrediction(prediction.Id)
+	if err != nil {
+		t.Fatalf("can't load prediction: %s", err)
+	}
+	assertBool(predictionLoaded.CreatedAt.IsZero(), false, t)
+	assertBool(predictionLoaded.StartedAt.IsZero(), true, t)
+
+	err = db.ClosePrediction(prediction, true)
+	assertNoError(err, t)
+}
